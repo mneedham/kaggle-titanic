df5d29f07bab068d7691fc6c7b0618e9e3ea62cc
Mark Needham <m.h.needham@gmail.com>
2013-11-09 10:07:47 +0000

    output formatting

--- titanic-ml-explore.py
+++ titanic-ml-explore.py
@@ -8,6 +8,12 @@ import itertools as it
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
+def all_combinations(coll):
+	return reduce(lambda acc, x: acc + list(it.combinations(coll, x)), range(1, len(coll) + 1), [])
+
+def right_justify(s):
+    return "%45s" % str(s)
+
 train_df = pd.read_csv("train.csv")
 train_df["Sex"] = train_df["Sex"].apply(lambda sex: 0 if sex == "male" else 1)
 train_df["Embarked"] = train_df["Embarked"].apply(lambda port: 0 if port == "S" else 1 if port == "C" else 2)
@@ -26,10 +32,9 @@ et = ExtraTreesClassifier(n_estimators=100, max_depth=None, min_samples_split=1,
 print(train_df.head())
 
 all_columns = ["Fare", "Sex", "Pclass", 'Embarked', "Age"]
-for columns in (list(it.combinations(all_columns, 2)) + 
-				list(it.combinations(all_columns, 3)) + 
-				list(it.combinations(all_columns, 4)) +
-				list(it.combinations(all_columns, 5))):
+
+
+for columns in all_combinations(all_columns):
 	labels = train_df["Survived"].values
 	features = train_df[list(columns)].values
 
@@ -43,5 +48,7 @@ for columns in (list(it.combinations(all_columns, 2)) +
 	et_score = cross_val_score(et, features_train, labels_train, n_jobs=-1).mean()
 	svm_score = cross_val_score(support_vector_machine, features_train, labels_train, n_jobs=-1).mean()
 	knn_score = cross_val_score(knn, features_train, labels_train, n_jobs=-1).mean()
+
+	columns = right_justify(columns)
 
 	print("{0} -> {5} (RF: {1}, ET: {2}, SVM: {3}, KNN: {4})".format(columns, rf_score, et_score, svm_score, knn_score, max([rf_score, et_score, svm_score, knn_score])))	
